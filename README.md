fe6fa05f53e13c74863285c9b4ad82877e98afab
Russell Brookman <hr.brookman@gmail.com>
2021-08-15 21:12:27 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # lego-hangman
+
+A basic hangman game. It checks if the letter you typed was correct, counts your number of guesses, and changes the image shown whenever you guess wrong or when you win the game. I used basic HTML and CSS for the design and JavaScript for the logic. 
+
+	
+# Prerequisites
+
+* None
+
+
+# Dependencies
+
+* none
+
+
+# Languages Used
+
+1. JavaScript
+
+2. jQuery
--- logic.js
+++ logic.js
@@ -1,11 +1,8 @@
-
 var words = ['superman', 'batman', 'green lantern', 'wonder woman', 'the flash', 'robin', 'zatana', 'hawkgirl', 'hawkman', 'cyborg', 'aqua man', 'lex luthor', 'brainiac', 'darkseid', 'general zod', 'krypton', 'lois lane', 'lana lang', 'the joker', 'ares', 'sinestro', 'captain cold', 'gorilla grodd', 'cat woman', 'cheetah', 'chloe sullivan', 'captain marvel', 'black adam', 'the penguin', 'the riddler', 'non', 'supergirl', 'gotham city', 'bane', 'oa', 'themyscira', 'green arrow', 'central city', 'star labs', 'fortress of solitude', 'doomsday', 'solomon grundy'];
 
 var theWord = null;
 var letterGuessed = null;
 var inTypedLetters = [];
-// var numBlanks = 0;
-// var blanksAndSuccesses = [];
 var rightL = [];
 var wrongL = [];
 var winCounter = 0;
@@ -16,7 +13,6 @@ var totalGuesses = 0;
 
 function startGame() {
   theWord = words[Math.floor(Math.random() * words.length)];  
-
   // Split the chosen word up into its individual letters.
   inTypedLetters = theWord.split("");
   rebuildWord();
@@ -142,11 +138,6 @@ function updateWinCounter() {
     document.getElementById("currentImg").src ="images/lego-superman-flying.jpg";
     alert("You Won!");
     document.querySelector("#winCount").innerHTML = winCounter;
-    // Update the image of the band on the page
-    // document.querySelector("#bandDiv").innerHTML =
-    //   "<img class='band-image' src='images/" +
-    //   this.wordsToPick[theWord].picture + "' alt='";
-    // trigger restart in the updatePage function.
     return true;
   }
   // game continues
@@ -162,123 +153,3 @@ document.onkeyup = function(event) {
   updatePage(letterGuessed);
 };
 
-  
-//   // Clears the wrong guesses from the previous round
-//   document.getElementById("wrongGuesses").innerHTML = wrongL.join(" ");
-// }
-  
-//   // compare matches
-// function checkLetters(letter) {
-  
-//   var letterInWord = false;
-  
-//   // does letter exists inside the array 
-//   for (var i = 0; i < numBlanks; i++) {
-//     if (theWord[i] === letter) {
-//       letterInWord = true;
-//     }
-//   }
-  
-//   // If the letter exists this will say where in the word it is
-//   if (letterInWord) {
-//     for (var j = 0; j < numBlanks; j++) {
-//   // Populate the blanksAndSuccesses with every instance of the letter.
-//       if (theWord[j] === letter) {
-//         blanksAndSuccesses[j] = letter;
-//       }
-//     }
-//   }
-//   else {
-//     // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
-//   wrongL.push(letter);
-//   numGuesses--;
-
-  // runImage () {
-  //   if (numGuesses == 7) {
-  //     guessSeven();
-  //   } else if (numGuesses == 6) {
-  //     guessSix();
-  //   } else if (numGuesses == 5) {
-  //     guessFive();
-  //   } else if (numGuesses == 4) {
-  //     guessFour();
-  //   } else if (numGuesses == 3) {
-  //     guessThree();
-  //   } else if (numGuesses == 2) {
-  //     guessTwo();
-  //   } else if (numGuesses == 1) {
-  //     guessOne();
-  //   } else {
-  //     guessZero();
-  //   }
-  // }
-
-// function guessSeven() {
-//   document.getElementById("legs").style.display = "block";
-// }
-// function guessSix() {
-//   document.getElementById("legs").sytle.display = "none";
-//   document.getElementById("body").sytle.display = "block";
-// }
-// function guessFive() {
-//   document.getElementById("body").sytle.display = "none";
-//   document.getElementById("arms").sytle.display = "block";
-// }
-// function guessFour() {
-//   document.getElementById("arms").sytle.display = "none";
-//   document.getElementById("oneHand").sytle.display = "block";
-// }
-// function guessThree() {
-//   document.getElementById("oneHand").sytle.display = "none";
-//   document.getElementById("bothHands").sytle.display = "block";
-// }
-// function guessTwo() {
-//   document.getElementById("bothHands").sytle.display = "none";
-//   document.getElementById("withHead").sytle.display = "block";
-// }
-// function guessOne() {
-//   document.getElementById("withHead").sytle.display = "none";
-//   document.getElementById("withCape").sytle.display = "block";
-// }
-// function guessZero() {
-//   document.getElementById("withCape").sytle.display = "none";
-//   document.getElementById("lostDry").sytle.display = "block";
-// }
-  
-// function roundComplete() {
- 
-//     // Update the HTML, IE: print or render
-//   document.getElementById("guessesLeft").innerHTML = numGuesses;
-//   document.getElementById("blanks").innerHTML = blanksAndSuccesses.join(" ");
-//   document.getElementById("wrongGuesses").innerHTML = wrongL.join(" ");
-   
-//     // If we have gotten all the letters to match the solution...
-//   if (inTypedLetters.toString() === blanksAndSuccesses.toString()) {
-//     winCounter++;
-//     alert("You win!");
-//     document.getElementById("wonEar").style.display = "inline";
-//     // Update the win counter & restart the game.
-//     document.getElementById("winCount").innerHTML = winCounter;
-//     startGame();
-//   }
-//   else if (numGuesses === 0) {
-//     looseCounter++;
-//     alert("You lose");
-//     guessZero();
-//       // Update the loose counter
-//     document.getElementById("looseCounter").innerHTML = looseCounter;
-//       // Restart the game.
-//     startGame();
-//   }
-// }
-
-// startGame();
-//   // capturing clicks.
-// document.onkeyup = function(event) {
-//  // clicks to lowercase letters.
-//   var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-//   // Run check.
-//   checkLetters(letterGuessed);
-//   // Run after each round.
-//   roundComplete();
-// };
